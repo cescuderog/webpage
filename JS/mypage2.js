0ffc42af1117b5cec8e808e8e8aa4a085ed4d4c1
Claudio Escudero Gamboa <107541289+cescuderog@users.noreply.github.com>
2022-07-01 12:48:12 +0200

    Add files via upload

---
+++ JS/mypage2.js
@@ -0,0 +1,37 @@
+function openForm() {
+  document.getElementById("popupForm").style.display = "block";
+}
+function closeForm() {
+  document.getElementById("popupForm").style.display = "none";
+}
+
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Gracias por tu mensaje! Trataré de responderte lo antes posible";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! Hubo un problema al enviar tu mensaje. Por favor intenta nuevamente"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! Hubo un problema al enviar tu mensaje. Por favor intenta nuevamente"
+  });
+}
+form.addEventListener("submit", handleSubmit)
